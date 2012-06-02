b18567f0f7495bf03313a9c2ef08f80076cbee03
janek richter <janek@openbug.org>
2012-06-02 16:52:49 +0200

    terminal theme added, exclude osx non-dotfile dir on other os'es

--- get.sh
+++ get.sh
@@ -7,12 +7,19 @@ TEMPDIR=`mktemp -d XXXXXX`
 if [ `uname` = "Linux" -o `uname` = "Darwin" ]; then
 	GET="curl -L"
 else
-	# openbsd
 	GET="ftp -o -"
 fi
 
 $GET https://github.com/sizowie/dotfiles/tarball/master | tar -C $TEMPDIR -xzf -
+
+# remove unused files
 rm -f $TEMPDIR/sizowie-*/README.md $TEMPDIR/sizowie-*/get.sh
+
+if [ `uname` != "Darwin" ]; then
+	# exclude osx files on other os'es
+	rm -rf $TEMPDIR/sizowie-*/osx
+fi
+
 cd $TEMPDIR/sizowie-* && tar -cf - . | (cd; tar -xvf -)
 cd -
 
